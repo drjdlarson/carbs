46b150763d2d85999cd66621eb8413509ec3c84e
Dawson Pierce <dbpierce@crimson.ua.edu>
2025-03-31 17:00:29 -0500

    Bug fixes.

--- src/carbs/extended_targets/GGIW_EKF.py
+++ src/carbs/extended_targets/GGIW_EKF.py
@@ -182,72 +182,83 @@ class GGIW_ExtendedKalmanFilter(ExtendedKalmanFilter):
         cur_IWdof = GGIW_obj.IWdof
         cur_IWshape = GGIW_obj.IWshape
 
-        W = np.size(meas, axis=1)
+        try: 
+            W = np.size(meas,axis=1)
+        except:
+            W = None
 
-        est_meas, meas_mat = self._est_meas(
-            timestep, cur_state, np.size(meas, axis=0), meas_fun_args
-        )
+        if W is not None: 
 
-        mean_meas = np.mean(meas, axis=1)
-        mean_meas = mean_meas.reshape((np.size(meas,axis=0)),1)
+            est_meas, meas_mat = self._est_meas(
+                timestep, cur_state, np.size(meas, axis=0), meas_fun_args
+            )
 
-        diff_Z = meas - mean_meas
-        Z = diff_Z @ diff_Z.T            # Essentially the scatter
+            mean_meas = np.mean(meas, axis=1)
+            mean_meas = mean_meas.reshape((np.size(meas,axis=0)),1)
 
-        X_hat = cur_IWshape * (cur_IWdof - 2 * GGIW_obj.d - 2)**(-1)
-        X_hat = (X_hat + X_hat.T)*0.5
+            diff_Z = meas - mean_meas
+            Z = diff_Z @ diff_Z.T            # Essentially the scatter
 
-        epsilon = mean_meas - meas_mat @ cur_state
+            cur_IWshape = 0.5*(cur_IWshape+cur_IWshape.T)
 
-        N = epsilon @ epsilon.T
+            X_hat = cur_IWshape * (cur_IWdof - 2 * GGIW_obj.d - 2)**(-1)
+            X_hat = (X_hat + X_hat.T)*0.5
 
-        S = meas_mat @ cur_cov @ meas_mat.T + X_hat / W 
-        S = (S + S.T) * 0.5
+            epsilon = mean_meas - meas_mat @ cur_state
 
-        Vs = la.cholesky(S)
-        det_S = la.det(Vs)
-        inv_sqrt_S = la.inv(Vs)
-        iS = inv_sqrt_S * inv_sqrt_S.T 
+            N = epsilon @ epsilon.T
 
-        K = cur_cov @ meas_mat.T @ iS 
-        
-        X_sqrt = sla.sqrtm(X_hat)
-        S_sqrt_inv = sla.sqrtm(iS)
+            cur_cov = 0.5 * (cur_cov + cur_cov.T)
 
-        N_hat = X_sqrt @ S_sqrt_inv @ N @ S_sqrt_inv.T @ X_sqrt.T
+            S = meas_mat @ cur_cov @ meas_mat.T + X_hat / W + self.meas_noise
+            S = (S + S.T) * 0.5 
 
-        next_alpha = cur_alpha + W
-        next_beta = cur_beta + 1
-        next_state = cur_state + K @ epsilon 
-        next_cov = cur_cov - K @ meas_mat @ cur_cov
-        next_IWdof = cur_IWdof + W
-        next_IWshape = cur_IWshape + N_hat + Z 
+            Vs = la.cholesky(S)
+            det_S = la.det(Vs)
+            inv_sqrt_S = la.inv(Vs)
+            iS = inv_sqrt_S * inv_sqrt_S.T 
 
-        next_dist = GGIW(alpha=next_alpha, beta=next_beta, mean=next_state, covariance=next_cov, IWdof=next_IWdof, IWshape=next_IWshape)
+            K = cur_cov @ meas_mat.T @ iS 
+            
+            X_sqrt = sla.sqrtm(X_hat)
+            S_sqrt_inv = sla.sqrtm(iS)
 
-        gam = next_alpha / next_beta
+            N_hat = X_sqrt @ S_sqrt_inv @ N @ S_sqrt_inv.T @ X_sqrt.T
 
-        
-        # Compute each term
-        term1  = (cur_IWdof - GGIW_obj.d - 1)/2 * np.log(np.linalg.det(cur_IWshape))
-        term2  = - (next_IWdof - GGIW_obj.d - 1)/2 * np.log(np.linalg.det(next_IWshape))
-        term3  = special.gammaln((next_IWdof - GGIW_obj.d - 1)/2)
-        term4  = - special.gammaln((cur_IWdof - GGIW_obj.d - 1)/2)
-        term5  = 0.5 * np.log(np.linalg.det(X_hat))
-        term6  = -0.5 * np.log(det_S)
-        term7  = special.gammaln(next_alpha)
-        term8  = -special.gammaln(cur_alpha)
-        term9  = cur_alpha * np.log(cur_beta)
-        term10 = - next_alpha * np.log(next_beta)
-        term11 = - ((W * np.log(np.pi) + np.log(W)) * GGIW_obj.d / 2)
+            next_alpha = cur_alpha + W
+            next_beta = cur_beta + 1
+            next_state = cur_state + K @ epsilon 
+            next_cov = cur_cov - K @ meas_mat @ cur_cov
+            next_IWdof = cur_IWdof + W
+            next_IWshape = cur_IWshape + N_hat + Z 
 
-        # Sum them up
-        meas_fit_prob = (
-            term1 + term2 + term3 + term4 + term5 + term6 + 
-            term7 + term8 + term9 + term10 + term11
-        )
+            next_dist = GGIW(alpha=next_alpha, beta=next_beta, mean=next_state, covariance=next_cov, IWdof=next_IWdof, IWshape=next_IWshape)
 
-        return (next_dist, meas_fit_prob)
+            gam = next_alpha / next_beta
+
+            
+            # Compute each term
+            term1  = (cur_IWdof - GGIW_obj.d - 1)/2 * np.log(np.linalg.det(cur_IWshape))
+            term2  = - (next_IWdof - GGIW_obj.d - 1)/2 * np.log(np.linalg.det(next_IWshape))
+            term3  = special.gammaln((next_IWdof - GGIW_obj.d - 1)/2)
+            term4  = - special.gammaln((cur_IWdof - GGIW_obj.d - 1)/2)
+            term5  = 0.5 * np.log(np.linalg.det(X_hat))
+            term6  = -0.5 * np.log(det_S)
+            term7  = special.gammaln(next_alpha)
+            term8  = -special.gammaln(cur_alpha)
+            term9  = cur_alpha * np.log(cur_beta)
+            term10 = - next_alpha * np.log(next_beta)
+            term11 = - ((W * np.log(np.pi) + np.log(W)) * GGIW_obj.d / 2)
+
+            # Sum them up
+            meas_fit_prob = (
+                term1 + term2 + term3 + term4 + term5 + term6 + 
+                term7 + term8 + term9 + term10 + term11
+            )
+
+            return (next_dist, meas_fit_prob)
+        else:
+            return (GGIW_obj,1)
 
     def _calc_meas_fit(self): #, meas, GGIW_pred, GGIW_upd, X_hat, inov_cov):
         
--- src/carbs/extended_targets/GGIW_RFS.py
+++ src/carbs/extended_targets/GGIW_RFS.py
@@ -250,39 +250,46 @@ class GGIW_PHD(RandomFiniteSetBase):
         Mix.weights = [self.prob_miss_detection * x for x in Mix.weights]
 
         UpdMix = self._correct_prob_density(timestep, parted_meas, self._Mixture, filt_args) 
-        UpdMix.add_components(Mix.alphas, Mix.betas, Mix.means, Mix.covariances, Mix.IWdofs, Mix.IWshapes, Mix.weights) # change for GGIW 
+        UpdMix.add_components(Mix.alphas, Mix.betas, Mix.means, Mix.covariances, Mix.IWdofs, Mix.IWshapes, Mix.weights) 
 
         self._Mixture = UpdMix
 
     def _correct_prob_density(self, timestep, parted_meas, probDensity, filt_args):
         # means = []
         # covariances = []
-        weights = []
 
-        Mix_temp = GGIWMixture()
+        if parted_meas:
 
-        det_weights = [self.prob_detection * x for x in probDensity.weights]
+            weights = []
 
-        for z in parted_meas: 
+            Mix_temp = GGIWMixture()
 
-            z = np.squeeze(z) 
+            det_weights = [self.prob_detection * x for x in probDensity.weights]
 
-            # w_lst = []
-            for jj in range(0, len(probDensity)):
-                cur_dist = probDensity[jj] 
-                (upd_dist, qz) = self.filter.correct(timestep, z, cur_dist, **filt_args) 
-                w = qz * det_weights[jj]
+            for z in parted_meas: 
 
-                Mix_temp.add_components(upd_dist.alpha, upd_dist.beta, upd_dist.mean, upd_dist.covariance, upd_dist.IWdof, upd_dist.IWshape, w)            
+                w_lst = []
 
-        w_lst = Mix_temp.weights 
-        weights.extend(
-            [x / (self.clutter_rate * self.clutter_den + sum(w_lst)) for x in w_lst]
-        )
+                z = np.squeeze(z) 
 
-        Mix_temp.weights = weights
+                for jj in range(0, len(probDensity)):
+                    cur_dist = probDensity[jj] 
+                    (upd_dist, qz) = self.filter.correct(timestep, z, cur_dist, **filt_args) 
+                    w = qz * det_weights[jj]
 
-        return Mix_temp
+                    Mix_temp.add_components(upd_dist.alpha, upd_dist.beta, upd_dist.mean, upd_dist.covariance, upd_dist.IWdof, upd_dist.IWshape, w)            
+
+                    w_lst.append(w)
+
+                weights.extend(
+                    [x / (self.clutter_rate * self.clutter_den + sum(w_lst)) for x in w_lst]
+                )
+
+            Mix_temp.weights = weights
+
+            return Mix_temp
+        else:
+            return probDensity
 
     def _prune(self):
         """Removes hypotheses below a threshold.
