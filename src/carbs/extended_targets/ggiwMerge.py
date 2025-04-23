ecd37be694bf39be8f3ac916313656c97fbdda04
Tuan Luong <tdluong@crimson.ua.edu>
2025-04-23 17:40:39 -0500

    implement merge in RFS code. NEED MORE TESTING

--- src/carbs/extended_targets/ggiwMerge.py
+++ src/carbs/extended_targets/ggiwMerge.py
@@ -42,8 +42,8 @@ def _gamma_cost_function(alpha:float, scalar:float)->float:
     return np.log(alpha) - scipy.special.digamma(alpha) + scalar
 
 
-def ggiw_merge(w:list, means:list, covs:list, alphas:list, betas:list, dofs:list,
-                scale:list, labels:list = None, opt_nu:bool = True, opt_alpha:bool = True) -> tuple:
+def ggiw_merge(w:list, means:list, covs:list, alphas:list, betas:list, IWdof:list,
+                IWshape:list, labels:list = None, opt_nu:bool = True, opt_alpha:bool = True) -> tuple:
     """
     Implementation to merge a group of GGIW components
 
@@ -59,10 +59,10 @@ def ggiw_merge(w:list, means:list, covs:list, alphas:list, betas:list, dofs:list
         float shape parameters of Gamma component of GGIW group
     betas : list
         float rate parameters of Gamma component of GGIW group
-    dofs : list
+    IWdof : list
         float degrees of freedom of IW component of GGIW group
-    scale : list
-        dxd numpy array scale matrix of IW component of GGIW group
+    shape : list
+        dxd numpy array shape matrix of IW component of GGIW group
     labels : list, optional
         lable of GGIW group (default to None)
     opt_nu : bool, optional
@@ -78,7 +78,7 @@ def ggiw_merge(w:list, means:list, covs:list, alphas:list, betas:list, dofs:list
 
     num_comp = len(w)
     d = means[0].shape[0]
-    shape_d = scale[0].shape[0]
+    shape_d = IWshape[0].shape[0]
 
     # Merge of Gaussian components
     w_merged = sum(w)
@@ -88,8 +88,8 @@ def ggiw_merge(w:list, means:list, covs:list, alphas:list, betas:list, dofs:list
     cov_merged = sum([n*(c + e@e.transpose()) for n,c,e in zip(w,covs,e_lst)])/w_merged
     
     # Merge IW components
-    exp_dof = sum(a*b for a,b in zip(w, dofs))/w_merged
-    inv_scale = [np.linalg.inv(x) for x in scale] # Pre-compute matrix inversion
+    exp_dof = sum(a*b for a,b in zip(w, IWdof))/w_merged
+    inv_scale = [np.linalg.inv(x) for x in IWshape] # Pre-compute matrix inversion
 
     if opt_nu:
         # Precompute terms for Eq. (9d) from [1] that are not dependent on input params
@@ -100,13 +100,13 @@ def ggiw_merge(w:list, means:list, covs:list, alphas:list, betas:list, dofs:list
         d_terms = np.array(range(1,shape_d+1),dtype='float')
         d_terms *= 0.5
         for ii in range(num_comp):
-            temp += w[ii] * (dofs[ii] - shape_d - 1) * inv_scale[ii]
+            temp += w[ii] * (IWdof[ii] - shape_d - 1) * inv_scale[ii]
             t5 += w[ii] * \
-                sum(scipy.special.digamma(np.repeat(((dofs[ii] - float(shape_d))/2),shape_d) - d_terms))
+                sum(scipy.special.digamma(np.repeat(((IWdof[ii] - float(shape_d))/2),shape_d) - d_terms))
 
         t4 = w_merged * np.linalg.slogdet(temp)[1]
 
-        t6 = sum(n * np.linalg.slogdet(sc)[1] for n,sc in zip(w, scale))
+        t6 = sum(n * np.linalg.slogdet(sc)[1] for n,sc in zip(w, IWshape))
         scalar = t3 - t4 + t5 - t6
 
         solve_res = scipy.optimize.root_scalar(_dof_cost_function, x0=exp_dof, \
@@ -122,7 +122,7 @@ def ggiw_merge(w:list, means:list, covs:list, alphas:list, betas:list, dofs:list
         dof_merged = exp_dof
     dof_merged = max(dof_merged, 2 * shape_d + 3)  # Numerical hack to ensure IW is well defined
 
-    temp = sum(n * (a - shape_d - 1) * inv_sc for n,a,inv_sc in zip(w, dofs, inv_scale))
+    temp = sum(n * (a - shape_d - 1) * inv_sc for n,a,inv_sc in zip(w, IWdof, inv_scale))
     scale_merged = w_merged * (dof_merged - shape_d - 1) * np.linalg.inv(temp)
     
     # Merge Gamma compoenents
